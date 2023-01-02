5307a30075a83dde75574c9eb32451df72e07dd4
azuma <054takafumi@gmail.com>
2023-01-02 12:21:09 +0900

    CSSのファイル位置を変更した

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import "./CSS/index.css";
 import { GameView } from "./View/GameView";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
